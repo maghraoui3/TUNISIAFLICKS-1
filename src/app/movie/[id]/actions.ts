25521493bf9475b5216aa75064bab7cbd9c164d2
Malek Maghraoui <malek.magraoui3@gmail.com>
2024-11-04 15:59:28 +0100

    .

---
+++ src/app/movie/[id]/actions.ts
@@ -0,0 +1,25 @@
+"server";
+
+"use server";
+
+export default async function getMovieData(id: string) {
+  const API_KEY = process.env.TMDB_API_KEY
+  if (!API_KEY) {
+    throw new Error("TMDB_API_KEY is not set")
+  }
+
+  try {
+    const res = await
+        fetch(
+            `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`,
+        //   `https://api.themoviedb.org/3/?api_key=${API_KEY}&language=en-US&page=1`,
+          { next: { revalidate: 3600 } }
+    )
+
+
+    return res.json()
+  } catch (error) {
+    console.error("Error fetching movies:", error)
+    throw error
+  }
+}
--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+import React, {useEffect, useState} from 'react'
+import getMovieData from './actions'
 
-function page({params}: {params: {id: string}}) {
+function Page({params}: {params: {id: string}}) {
+  const [data, setData] = useState(null)
+  const [loader, setLoader] = useState(true)
+
+  useEffect(() => {
+    async function fetchMovieData() {
+      try {
+        const moviesData = await getMovieData(params.id)
+        setData(moviesData)
+        setLoader(false)
+        console.log(moviesData)
+      } catch (error) {
+        console.error("Error fetching movie:", error)
+        setLoader(false)
+      }
+    }
+
+    fetchMovieData()
+  }, [])
   return (
-    <div>movie Page with id {params.id}</div>
+    <div className='w-full'>
+      {/* <p>movie Page with id {params.id}</p> */}
+      <iframe src={`https://multiembed.mov/?video_id=${params.id}&tmdb=1`} className='w-full h-full' allowFullScreen></iframe>
+    </div>
   )
 }
 
-export default page
+export default Page

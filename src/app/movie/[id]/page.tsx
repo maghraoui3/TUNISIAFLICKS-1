b9dc487832135c4dbc9641e2034c3059752749e7
Malek Maghraoui <malek.magraoui3@gmail.com>
2024-11-06 14:17:44 +0100

    .

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -7,6 +7,8 @@ import Autoplay from 'embla-carousel-autoplay'
 import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'
 import './carousel.style.css'
+import { FaHeart } from "react-icons/fa6";
+
 
 function Page({ params }: { params: { id: string } }) {
 
@@ -66,8 +68,30 @@ function Page({ params }: { params: { id: string } }) {
           <div className='absolute bottom-0 h-2/6 w-full' style={{ background: 'linear-gradient(transparent, #0d0c0f)' }}/>
           <div className='absolute left-0 bottom-0 h-full w-1/2' style={{ background: 'linear-gradient(to left, transparent, #0d0c0f)' }}/>
           <div className='w-full h-full absolute left-0 bottom-0'/>
-          <div className='absolute pl-10 top-1/4 w-full h-full'>
-            <Image src={`https://image.tmdb.org/t/p/original${logos[1].file_path}`} className='w-80' width={500} height={100} alt="" />
+          <div className='absolute pl-10 top-1/4 w-2/5 h-full'>
+            {/* <Image src={`https://image.tmdb.org/t/p/original${logos[1].file_path}`} className='w-80' width={500} height={100} alt="Movie LOGO" /> */}
+            {logos.length == 0 ? null :
+              <Image src={`https://image.tmdb.org/t/p/original${logos.filter((item) => item.iso_639_1 == 'en' )[0].file_path || logos[0].file_path }`} className='w-80' width={500} height={100} alt="Movie LOGO" />}
+            <p className='flex items-center gap-3 mt-5'>
+              <p><Image src={`https://flagsapi.com/${data.origin_country}/flat/32.png`} width={32} height={32} alt='slm'/></p>
+              <p className='flex gap-1 items-center'><FaHeart className='text-red-500'/><p className='text-red-500 font-bold'>{Math.round(data.vote_average*10)}%</p> Likes</p>
+              <p>•</p>
+              <p>{data.release_date.substring(0, 4)}</p>
+              {
+                data.adult &&
+                <div className='flex gap-3'>
+                  <p>•</p>
+                  <p className='bg-red-500 px-3 rounded-xl'>+18</p>
+                </div>
+              }
+              <p>•</p>
+              <p>{data.genres.map((item, index) => data.genres.length-1 !== index ? item.name + ", " : item.name )}</p>
+            </p>
+            <p className='mt-3 text-gray-200'>{data.overview}</p>
+            <div className='mt-3'>
+              <p>CASTS:</p>
+              <div></div>
+            </div>
           </div>
         </div>
 
@@ -86,7 +110,7 @@ type PropType = {
 const EmblaCarousel: React.FC<PropType> = (props) => {
   const { slides, options } = props
   const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay({ delay: 3000 })])
-  console.log(slides)
+  // console.log(slides)
   return (
     <section className="embla">
       <div className="embla__viewport" ref={emblaRef}>

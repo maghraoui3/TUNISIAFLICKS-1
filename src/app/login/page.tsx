3bdfe4e2965023aa7fd4d2f536026fc8bd2dd072
Malek Maghraoui <malek.magraoui3@gmail.com>
2025-01-04 12:36:11 +0100

    just &apos;` 🌝

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -76,7 +76,7 @@ export default function LoginPage() {
         </CardContent>
         <CardFooter className="flex justify-center">
           <p className="text-sm text-gray-600">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="text-blue-500 hover:underline">
               Sign up
             </Link>
